2d5be891d8e6c872978f44bf0ff62fb090dcfcd6
GUTZD <55847110+GUTZD@users.noreply.github.com>
2019-09-26 18:58:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+const Discord = require("discord.js");
+const client = new Discord.Client;
 const prefix = "!" //this is the last prefix 
+
+client.on("message", message => {
+if(message.content == "ping") {
+  message.reply("pong");
+}
+});
